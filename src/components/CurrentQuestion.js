4f0bdc086f9566574f1aa1949a5e21cbbbfcddc4
CarolineF3 <caroline.fahlgren@gmail.com>
2021-04-12 14:22:34 +0200

    currentquestion js + css

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,16 +1,34 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { quiz } from '../reducers/quiz';
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+  const quizOver = useSelector((state) => state.quiz.quizOver);
+  console.log(quizOver);
+  
+  const dispatch = useDispatch();
 
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
   }
 
   return (
-    <div>
-      <h1>Question: {question.questionText}</h1>
-    </div>
+    <>
+      <div className='question-container'>
+        <h1>Question: {question.questionText}</h1>
+        <p className='question-counter'>Current question {}</p>
+      </div>
+      <div className='button-container'>
+        <div className='buttons-inner'>
+            <button onClick={() => dispatch(quiz.actions.goToNextQuestion())} className='answer-button'>{question.options[0]}</button>
+           <button onClick={() => dispatch(quiz.actions.goToNextQuestion())} className='answer-button'>{question.options[1]}</button>
+        </div>
+        <div className='buttons-inner'>
+            <button onClick={() => dispatch(quiz.actions.goToNextQuestion())} className='answer-button'>{question.options[2]}</button>
+            <button onClick={() => dispatch(quiz.actions.goToNextQuestion())} className='answer-button'>{question.options[3]}</button>
+        </div> 
+      </div>
+    </>
   )
 }
