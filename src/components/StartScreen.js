14192ce11dee2ca1d6eccd7e84f13d82621cf83f
Rasmus Persson <rrasmuspersson@gmail.com>
2021-04-12 13:09:19 +0200

    Added a StartScreen component and a css file to go along with it.

---
+++ src/components/StartScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import StartScreenStyle from "./StartScreenStyle.css";
+
+const StartScreen = () => {
+  return (
+    <div className="start-screen-container">
+      <div>Some text goes here</div>
+      <button>Start Quiz</button>
+    </div>
+  );
+};
+
+export default StartScreen;
