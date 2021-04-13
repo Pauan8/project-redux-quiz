97655c43d4a5db20ee9f9341a86b548da233b89a
Aepera <astridepersson@gmail.com>
2021-04-13 09:48:37 +0200

    fixed buttons onclick

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux'
 
 import './SummaryStyle.css';
+import { quiz } from '../reducers/quiz'
 
 export const Summary = () => {
+
+  const dispatch = useDispatch()
+
   return (
     <section className="summary">
       <div className="summary-card">
         <h1 className="summary-card__heading">Summary</h1>
         <p className="summary-card__details">Your correct answers: 4/5</p>
         <p className="summary-card__precentage"> 20% accuracy </p>
-        <button type="button" className="play-again-btn">Play again</button>
+        <button onClick={() => dispatch(quiz.actions.restart())} type="button" className="play-again-btn">Play again</button>
       </div>
     </section>
   );
